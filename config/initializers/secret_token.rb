808b9969f60dd1a4be7c0f9394787eacb58dcb8f
Justin Burris <justinbburris@gmail.com>
2013-04-20 15:01:34 -0400

    ardusat init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ardusat::Application.config.secret_token = '7dddf6352208dbe7f290a51a2e1ce87b87d3ad71310ea0cca4104bdc63daeedd39400328e512b037cb30297fde69d35e1da55902eb257131386cd9781b1ec6a1'
